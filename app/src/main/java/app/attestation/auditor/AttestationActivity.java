f71e4c5456cb7c4a8c0c51ec83edb061f4e8b7d7
Daniel Micay <danielmicay@gmail.com>
2018-08-15 17:02:46 -0400

    no need to keep around supportedModels

--- app/src/main/java/app/attestation/auditor/AttestationActivity.java
+++ app/src/main/java/app/attestation/auditor/AttestationActivity.java
@@ -75,10 +75,10 @@ public class AttestationActivity extends AppCompatActivity {
     private int backgroundResource;
     private boolean canSubmitSample;
 
-    private static final ImmutableSet<String> supportedModels = ImmutableSet.of(
+    private static final boolean isSupportedAuditee = ImmutableSet.of(
             "BKL-L04", "G8441", "H3113", "H3123", "H4113", "Nokia 6.1", "Nokia 7 plus", "Pixel 2",
-            "Pixel 2 XL", "SM-G960F", "SM-G960U", "SM-G965F", "SM-G965U", "SM-G965U1", "SM-G965W");
-    private static final boolean isSupportedAuditee = supportedModels.contains(Build.MODEL);
+            "Pixel 2 XL", "SM-G960F", "SM-G960U", "SM-G965F", "SM-G965U", "SM-G965U1", "SM-G965W")
+            .contains(Build.MODEL);
 
     private static int getFirstApiLevel() {
         return Integer.parseInt(SystemProperties.get("ro.product.first_api_level",
