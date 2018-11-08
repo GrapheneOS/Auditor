9c468aedafdf94998e1116eed131ed49046f890c
Daniel Micay <danielmicay@gmail.com>
2018-11-08 15:59:56 -0500

    add Xiaomi Mi A2 Lite support

--- app/src/main/java/app/attestation/auditor/AttestationProtocol.java
+++ app/src/main/java/app/attestation/auditor/AttestationProtocol.java
@@ -272,6 +272,8 @@ class AttestationProtocol {
                     new DeviceInfo(R.string.device_blackberry_key2, 2, 3, true, true))
             .put("1194659B40EA291245E54A3C4EC4AA5B7077BD244D65C7DD8C0A2DBB9DB1FB35",
                     new DeviceInfo(R.string.device_bq_aquaris_x2_pro, 2, 3, true, false))
+            .put("6FA710B639848C9D47378937A1AFB1B6A52DDA738BEB6657E2AE70A15B40541A",
+                    new DeviceInfo(R.string.device_xiaomi_mi_a2_lite, 2, 3, true, false))
             .build();
 
     private static byte[] getChallengeIndex(final Context context) {
