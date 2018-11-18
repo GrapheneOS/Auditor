b4083aa5ea72332004e0fa6314b4aa11ee08f8f8
Daniel Micay <danielmicay@gmail.com>
2018-11-18 18:19:48 -0500

    add HTC 2Q55100 support

--- app/src/main/java/app/attestation/auditor/AttestationActivity.java
+++ app/src/main/java/app/attestation/auditor/AttestationActivity.java
@@ -92,6 +92,7 @@ public class AttestationActivity extends AppCompatActivity {
             "H4113",
             "H8216",
             "H8314",
+            "HTC 2Q55100",
             "Mi A2",
             "Mi A2 Lite",
             "Nokia 6.1",
