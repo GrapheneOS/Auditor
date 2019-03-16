56ffeaaad3b604e98a2b8001f36261dee5a89311
Daniel Micay <danielmicay@gmail.com>
2019-03-16 15:32:56 -0400

    sync with upstream attestation library

--- app/src/main/java/app/attestation/auditor/attestation/AuthorizationList.java
+++ app/src/main/java/app/attestation/auditor/attestation/AuthorizationList.java
@@ -185,6 +185,7 @@ public class AuthorizationList {
     private Date creationDateTime;
     private Integer origin;
     private boolean rollbackResistant;
+    private boolean rollbackResistance;
     private RootOfTrust rootOfTrust;
     private Integer osVersion;
     private Integer osPatchLevel;
@@ -269,6 +270,9 @@ public class AuthorizationList {
                     break;
                 case KM_TAG_ROLLBACK_RESISTANT & KEYMASTER_TAG_TYPE_MASK:
                     rollbackResistant = true;
+                    break;
+                case KM_TAG_ROLLBACK_RESISTANCE & KEYMASTER_TAG_TYPE_MASK:
+                    rollbackResistance = true;
                     break;
                 case KM_TAG_AUTH_TIMEOUT & KEYMASTER_TAG_TYPE_MASK:
                     authTimeout = Asn1Utils.getIntegerFromAsn1(value);
@@ -535,6 +539,10 @@ public class AuthorizationList {
         return rollbackResistant;
     }
 
+    public boolean isRollbackResistance() {
+        return rollbackResistance;
+    }
+
     public RootOfTrust getRootOfTrust() {
         return rootOfTrust;
     }
@@ -673,6 +681,10 @@ public class AuthorizationList {
 
         if (rollbackResistant) {
             s.append("\nRollback resistant: true");
+        }
+
+        if (rollbackResistance) {
+            s.append("\nRollback resistance: true");
         }
 
         if (rootOfTrust != null) {
