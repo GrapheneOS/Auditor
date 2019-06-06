ceaa380045bc3ef4db344a9dc49b40f45aa00702
Daniel Micay <danielmicay@gmail.com>
2019-06-06 08:33:59 -0400

    add verifiedBootHash field to attestation library

--- app/src/main/java/app/attestation/auditor/attestation/README.md
+++ app/src/main/java/app/attestation/auditor/attestation/README.md
@@ -1,3 +1,3 @@
-This library code comes from the [Android Compatibility Test
+This library code is based on code from the [Android Compatibility Test
 Suite](https://android.googlesource.com/platform/cts/+/57d89b24d452e7188ce4bf90be36e983118a3359/tests/security/src/android/keystore/cts)
-and is Apache2 licensed.
+and is Apache2 licensed. Various changes have been made to it here.
--- app/src/main/java/app/attestation/auditor/attestation/RootOfTrust.java
+++ app/src/main/java/app/attestation/auditor/attestation/RootOfTrust.java
@@ -27,6 +27,7 @@ public class RootOfTrust {
     private static final int VERIFIED_BOOT_KEY_INDEX = 0;
     private static final int DEVICE_LOCKED_INDEX = 1;
     private static final int VERIFIED_BOOT_STATE_INDEX = 2;
+    private static final int VERIFIED_BOOT_HASH_INDEX = 3;
 
     public static final int KM_VERIFIED_BOOT_VERIFIED = 0;
     public static final int KM_VERIFIED_BOOT_SELF_SIGNED = 1;
@@ -36,6 +37,7 @@ public class RootOfTrust {
     private final byte[] verifiedBootKey;
     private final boolean deviceLocked;
     private final int verifiedBootState;
+    private final byte[] verifiedBootHash;
 
     public RootOfTrust(ASN1Encodable asn1Encodable) throws CertificateParsingException {
         if (!(asn1Encodable instanceof ASN1Sequence)) {
@@ -49,6 +51,12 @@ public class RootOfTrust {
         deviceLocked = Asn1Utils.getBooleanFromAsn1(sequence.getObjectAt(DEVICE_LOCKED_INDEX));
         verifiedBootState =
                 Asn1Utils.getIntegerFromAsn1(sequence.getObjectAt(VERIFIED_BOOT_STATE_INDEX));
+        if (sequence.size() < 4) {
+            verifiedBootHash = null;
+            return;
+        }
+        verifiedBootHash =
+                Asn1Utils.getByteArrayFromAsn1(sequence.getObjectAt(VERIFIED_BOOT_HASH_INDEX));
     }
 
     public static String verifiedBootStateToString(int verifiedBootState) {
@@ -76,6 +84,10 @@ public class RootOfTrust {
 
     public int getVerifiedBootState() {
         return verifiedBootState;
+    }
+
+    public byte[] getVerifiedBootHash() {
+        return verifiedBootHash;
     }
 
     @Override
