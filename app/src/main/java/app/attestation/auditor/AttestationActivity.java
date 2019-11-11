c662ff6c404202c26ee28c79dbe1b6fa4d183ce8
Daniel Micay <danielmicay@gmail.com>
2019-11-10 20:22:33 -0500

    add SM-J337AZ support

--- app/src/main/java/app/attestation/auditor/AttestationActivity.java
+++ app/src/main/java/app/attestation/auditor/AttestationActivity.java
@@ -135,6 +135,7 @@ public class AttestationActivity extends AppCompatActivity {
             "SM-G965W",
             "SM-G975F",
             "SM-J337A",
+            "SM-J337AZ",
             "SM-J720F",
             "SM-M205F",
             "SM-N960F",
