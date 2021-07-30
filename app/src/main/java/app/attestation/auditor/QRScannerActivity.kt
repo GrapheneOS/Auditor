1bb36e78c52787a111c3fb23d3cfba8dcf45289d
Pratyush <codelab@pratyush.dev>
2021-07-30 12:02:59 +0530

    replaced barcodescanner implementation with camerax and zxing.
    uses of https://github.com/dm77/barcodescanner library has been replaced with camerax.
    
    see https://github.com/zxing/zxing
    and https://developer.android.com/training/camerax

---
+++ app/src/main/java/app/attestation/auditor/QRScannerActivity.kt
@@ -0,0 +1,85 @@
+package app.attestation.auditor
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Size
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+
+class QRScannerActivity : AppCompatActivity() {
+
+    public override fun onCreate(state: Bundle?) {
+        super.onCreate(state)
+        setContentView(R.layout.activity_qrscanner)
+        startCamera()
+    }
+
+
+    private fun startCamera() {
+        val contentFrame = findViewById<PreviewView>(R.id.content_frame)
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+        val cameraController = LifecycleCameraController(this)
+        cameraController.bindToLifecycle(this)
+        cameraController.cameraSelector = cameraSelector
+
+        cameraProviderFuture.addListener(
+            {
+                val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+                val preview = Preview.Builder()
+                    .build()
+                    .also {
+                        it.setSurfaceProvider(contentFrame.surfaceProvider)
+                    }
+
+                val imageAnalysis = ImageAnalysis.Builder()
+                    .setTargetResolution(Size(1280, 720))
+                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                    .build()
+
+                imageAnalysis.setAnalyzer(
+                    ContextCompat.getMainExecutor(this),
+                    QRCodeImageAnalyzer { response ->
+                        if (response != null) {
+                            handleResult(response)
+                        }
+                    }
+                )
+                try {
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        this,
+                        cameraSelector,
+                        preview,
+                        imageAnalysis
+                    )
+
+                } catch (exc: Exception) {
+                }
+
+            },
+            ContextCompat.getMainExecutor(this)
+        )
+    }
+
+
+    private fun handleResult(rawResult : String) {
+        val result = Intent()
+        result.putExtra(EXTRA_SCAN_RESULT, rawResult)
+        setResult(Activity.RESULT_OK, result)
+        finish()
+    }
+
+    companion object {
+        const val EXTRA_SCAN_RESULT = "app.attestation.auditor.SCAN_RESULT"
+    }
+}
