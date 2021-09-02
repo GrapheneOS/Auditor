fe3a7a44c8811fe2b372d65f29ca57d85b5d5726
Daniel Micay <danielmicay@gmail.com>
2021-09-02 19:38:23 -0400

    drop unnecessary check for image format

--- app/src/main/java/app/attestation/auditor/QRCodeImageAnalyzer.kt
+++ app/src/main/java/app/attestation/auditor/QRCodeImageAnalyzer.kt
@@ -17,40 +17,35 @@ import java.util.EnumMap
 class QRCodeImageAnalyzer(private val listener: (qrCode: String?) -> Unit) : Analyzer {
 
     override fun analyze(image: ImageProxy) {
-        if (image.format == ImageFormat.YUV_420_888
-            || image.format == ImageFormat.YUV_422_888
-            || image.format == ImageFormat.YUV_444_888
-        ) {
-            val byteBuffer = image.planes[0].buffer
-            val imageData = ByteArray(byteBuffer.capacity())
-            byteBuffer[imageData]
-            val source = PlanarYUVLuminanceSource(
-                imageData,
-                image.width, image.height,
-                0, 0,
-                image.width, image.height,
-                false
+        val byteBuffer = image.planes[0].buffer
+        val imageData = ByteArray(byteBuffer.capacity())
+        byteBuffer[imageData]
+        val source = PlanarYUVLuminanceSource(
+            imageData,
+            image.width, image.height,
+            0, 0,
+            image.width, image.height,
+            false
+        )
+        val binaryBitmap = BinaryBitmap(HybridBinarizer(source))
+        try {
+            val supportedHints: MutableMap<DecodeHintType, Any> = EnumMap(
+                DecodeHintType::class.java
             )
-            val binaryBitmap = BinaryBitmap(HybridBinarizer(source))
-            try {
-                val supportedHints: MutableMap<DecodeHintType, Any> = EnumMap(
-                    DecodeHintType::class.java
-                )
-                supportedHints[DecodeHintType.POSSIBLE_FORMATS] = listOf(BarcodeFormat.QR_CODE)
-                val result = MultiFormatReader().decode(
-                    binaryBitmap,
-                    supportedHints
-                )
-                listener.invoke(result.text)
-            } catch (e: FormatException) {
-                e.fillInStackTrace()
-            } catch (e: ChecksumException) {
-                e.fillInStackTrace()
-            } catch (e: NotFoundException) {
-                e.fillInStackTrace()
-            } catch (e: ArrayIndexOutOfBoundsException) {
-                e.fillInStackTrace()
-            }
+            supportedHints[DecodeHintType.POSSIBLE_FORMATS] = listOf(BarcodeFormat.QR_CODE)
+            val result = MultiFormatReader().decode(
+                binaryBitmap,
+                supportedHints
+            )
+            listener.invoke(result.text)
+        } catch (e: FormatException) {
+            e.fillInStackTrace()
+        } catch (e: ChecksumException) {
+            e.fillInStackTrace()
+        } catch (e: NotFoundException) {
+            e.fillInStackTrace()
+        } catch (e: ArrayIndexOutOfBoundsException) {
+            e.fillInStackTrace()
         }
         image.close()
     }
