8357e1da9afc5f207661fe0e1066394acdb97443
Daniel Micay <danielmicay@gmail.com>
2021-09-15 06:21:57 -0400

    clean up SystemProperties implementation

--- app/src/main/java/app/attestation/auditor/SystemProperties.java
+++ app/src/main/java/app/attestation/auditor/SystemProperties.java
@@ -5,17 +5,12 @@ import java.util.Scanner;
 
 class SystemProperties {
     public static String get(final String key, final String def) {
-        Scanner scanner = null;
         try {
             final Process process = new ProcessBuilder("getprop", key, def).start();
-            scanner = new Scanner(process.getInputStream());
-            return scanner.nextLine().trim();
-        } catch (IOException e) {
-            return def;
-        } finally {
-            if (scanner != null) {
-                scanner.close();
+            try (Scanner scanner = new Scanner(process.getInputStream())) {
+                return scanner.nextLine().trim();
             }
-        }
+        } catch (IOException e) {}
+        return def;
     }
 }
