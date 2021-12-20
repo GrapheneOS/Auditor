3f7c1732eca15f7c54713b8cc51085c4ed5178c1
Daniel Micay <danielmicay@gmail.com>
2021-12-20 14:18:33 -0500

    increment version to 39

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "app.attestation.auditor"
         minSdk = 26
         targetSdk = 31
-        versionCode = 38
+        versionCode = 39
         versionName = versionCode.toString()
         resConfigs("en")
     }
