703333c15cccdd0f1320e4fba50367f2c3637c34
Daniel Micay <danielmicay@gmail.com>
2022-04-12 09:58:27 -0400

    increment version to 45

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "app.attestation.auditor"
         minSdk = 26
         targetSdk = 32
-        versionCode = 44
+        versionCode = 45
         versionName = versionCode.toString()
         resourceConfigurations.add("en")
     }
