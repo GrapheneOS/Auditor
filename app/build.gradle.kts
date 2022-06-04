17e29bb833e7eb24b858102a42a586c29ed38e8b
Daniel Micay <danielmicay@gmail.com>
2022-06-04 11:01:52 -0400

    update AndroidX appcompat library to 1.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("androidx.appcompat:appcompat:1.4.2")
     implementation("androidx.biometric:biometric:1.1.0")
     implementation("androidx.preference:preference:1.2.0")
     implementation("com.google.android.material:material:1.6.1")
