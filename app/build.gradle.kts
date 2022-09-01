9dacea25074311846c445a9fc2a89e51ab6234fa
Daniel Micay <danielmicay@gmail.com>
2022-09-01 06:41:48 -0400

    update appcompat to 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,13 +65,16 @@ android {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.4.2")
+    implementation("androidx.appcompat:appcompat:1.5.0")
     implementation("androidx.biometric:biometric:1.1.0")
     implementation("androidx.preference:preference:1.2.0")
     implementation("com.google.android.material:material:1.6.1")
     implementation("com.google.guava:guava:31.1-android")
     implementation("com.google.zxing:core:3.5.0")
     implementation("org.bouncycastle:bcpkix-jdk18on:1.71.1")
+
+    // work around conflict
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
 
     val cameraVersion = "1.2.0-beta01"
     implementation("androidx.camera:camera-core:$cameraVersion")
