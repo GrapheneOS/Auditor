2c3b1ceb8df526f9f71a8072b2299533ba723bb0
Daniel Micay <danielmicay@gmail.com>
2022-09-12 20:15:34 -0400

    update appcompat to 1.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.5.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("androidx.biometric:biometric:1.1.0")
     implementation("androidx.preference:preference:1.2.0")
     implementation("com.google.android.material:material:1.6.1")
