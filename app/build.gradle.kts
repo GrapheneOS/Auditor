c5c5ab26fb9525bdb7850545778d8e09156a35ef
Daniel Micay <danielmicay@gmail.com>
2022-09-29 14:41:12 -0400

    update CameraX to 1.2.0-beta02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,7 +78,7 @@ dependencies {
     // work around conflict
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
 
-    val cameraVersion = "1.2.0-beta01"
+    val cameraVersion = "1.2.0-beta02"
     implementation("androidx.camera:camera-core:$cameraVersion")
     implementation("androidx.camera:camera-camera2:$cameraVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraVersion")
