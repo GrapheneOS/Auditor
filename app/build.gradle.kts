8b29a3904c08269dff7daacdc6ab21d110a562a6
Daniel Micay <danielmicay@gmail.com>
2022-12-09 06:44:21 -0500

    update CameraX to 1.3.0-alpha02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,7 +102,7 @@ dependencies {
     // work around conflict
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
 
-    val cameraVersion = "1.3.0-alpha01"
+    val cameraVersion = "1.3.0-alpha02"
     implementation("androidx.camera:camera-core:$cameraVersion")
     implementation("androidx.camera:camera-camera2:$cameraVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraVersion")
