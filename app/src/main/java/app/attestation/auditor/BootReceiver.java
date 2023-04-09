53548675c9a8b189f23e8b0566036f955e07d6fc
r3g_5z <june@girlboss.ceo>
2023-04-09 13:39:20 -0400

    silence UnsafeProtectedBroadcastReceiver lint
    Signed-off-by: r3g_5z <june@girlboss.ceo>

--- app/src/main/java/app/attestation/auditor/BootReceiver.java
+++ app/src/main/java/app/attestation/auditor/BootReceiver.java
@@ -1,10 +1,12 @@
 package app.attestation.auditor;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
 public class BootReceiver extends BroadcastReceiver {
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
     @Override
     public void onReceive(final Context context, final Intent intent) {
         RemoteVerifyJob.restore(context);
