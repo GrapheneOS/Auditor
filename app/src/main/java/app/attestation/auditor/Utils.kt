192f9768ce81b938be829f01263c3616154923f8
smdyv <88971341+smdyv@users.noreply.github.com>
2023-04-15 16:32:16 +0200

    refactor: Utils to idiomatic Kotlin

--- app/src/main/java/app/attestation/auditor/Utils.kt
+++ app/src/main/java/app/attestation/auditor/Utils.kt
@@ -1,16 +1,11 @@
 package app.attestation.auditor
 
 import com.google.common.io.BaseEncoding
-import java.util.Locale
 
 internal object Utils {
     @JvmStatic
     fun logFormatBytes(bytes: ByteArray): String {
-        return String.format(
-            Locale.US,
-            "%d binary bytes logged here as base64 (%s)",
-            bytes.size,
-            BaseEncoding.base64().encode(bytes)
-        )
+        val encodedBytes = BaseEncoding.base64().encode(bytes)
+        return "${bytes.size} binary bytes logged here as base64 ($encodedBytes)"
     }
 }
