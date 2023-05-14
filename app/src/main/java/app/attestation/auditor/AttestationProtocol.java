7a5acc87324fd3987ed4bf8eb4bbf55e7642cfc5
Daniel Micay <danielmicay@gmail.com>
2023-05-13 20:51:17 -0400

    remove unused import

--- app/src/main/java/app/attestation/auditor/AttestationProtocol.java
+++ app/src/main/java/app/attestation/auditor/AttestationProtocol.java
@@ -53,7 +53,6 @@ import java.security.cert.Certificate;
 import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
-import java.security.cert.CertificateParsingException;
 import java.security.cert.X509Certificate;
 import java.security.spec.ECGenParameterSpec;
 import java.util.ArrayList;
