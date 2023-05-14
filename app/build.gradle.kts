539a22fe3724e62dc86cb2f7bfe1a9af72523d0e
Daniel Micay <danielmicay@gmail.com>
2023-05-13 20:23:44 -0400

    increment version to 71

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
         applicationId = "app.attestation.auditor"
         minSdk = 29
         targetSdk = 33
-        versionCode = 70
+        versionCode = 71
         versionName = versionCode.toString()
         resourceConfigurations.add("en")
     }
