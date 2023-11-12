03602ab5809bfe2c233741fcd541f27f2989d700
Daniel Micay <daniel.micay@grapheneos.org>
2023-11-12 17:52:19 -0500

    update CameraX to 1.3.0
    Auditor will be following the stable release channel of CameraX from
    this point onwards since it doesn't get much benefit out of using the
    bleeding edge releases with the latest device compatibility workarounds.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -113,7 +113,7 @@ dependencies {
     implementation("com.google.zxing:core:3.5.2")
     implementation("org.bouncycastle:bcprov-jdk18on:1.76")
 
-    val cameraVersion = "1.3.0-rc02"
+    val cameraVersion = "1.3.0"
     implementation("androidx.camera:camera-core:$cameraVersion")
     implementation("androidx.camera:camera-camera2:$cameraVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraVersion")
