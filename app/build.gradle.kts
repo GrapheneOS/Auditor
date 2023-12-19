7f9c438590f62879582b3c03f80a7431df5593b2
Daniel Micay <daniel.micay@grapheneos.org>
2023-12-19 10:09:05 -0500

    increment version to 79

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
         applicationId = "app.attestation.auditor"
         minSdk = 29
         targetSdk = 34
-        versionCode = 78
+        versionCode = 79
         versionName = versionCode.toString()
         resourceConfigurations.add("en")
     }
