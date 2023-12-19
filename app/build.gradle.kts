e0d3564feee60510bf1ec52ebeb429f814e3eee2
Daniel Micay <daniel.micay@grapheneos.org>
2023-12-19 07:59:08 -0500

    update Guava to 33.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -110,7 +110,7 @@ dependencies {
     implementation("androidx.biometric:biometric:1.1.0")
     implementation("androidx.preference:preference:1.2.1")
     implementation("com.google.android.material:material:1.10.0")
-    implementation("com.google.guava:guava:32.1.3-android")
+    implementation("com.google.guava:guava:33.0.0-android")
     implementation("com.google.zxing:core:3.5.2")
     implementation("org.bouncycastle:bcprov-jdk18on:1.77")
 
