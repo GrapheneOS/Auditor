07c3efaa1186702b57b43958250943fff4a6fdf6
Daniel Micay <daniel.micay@grapheneos.org>
2024-05-20 18:04:57 -0400

    remove unused import

--- app/src/main/java/app/attestation/auditor/SubmitSampleJob.java
+++ app/src/main/java/app/attestation/auditor/SubmitSampleJob.java
@@ -1,6 +1,5 @@
 package app.attestation.auditor;
 
-import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
