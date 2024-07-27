d111ef53dcdf7b2006cb079fb245d55d9240fa99
Daniel Micay <daniel.micay@grapheneos.org>
2024-07-27 11:50:02 -0400

    raise minimum OS version for verification to 12

--- app/src/main/java/app/attestation/auditor/AttestationProtocol.java
+++ app/src/main/java/app/attestation/auditor/AttestationProtocol.java
@@ -234,7 +234,7 @@ class AttestationProtocol {
     private static final byte AUDITOR_APP_VARIANT_DEBUG = 2;
 
     private static final int AUDITOR_APP_MINIMUM_VERSION = 47;
-    private static final int OS_VERSION_MINIMUM = 100000;
+    private static final int OS_VERSION_MINIMUM = 120000;
     private static final int OS_PATCH_LEVEL_MINIMUM = 201909;
     private static final int VENDOR_PATCH_LEVEL_MINIMUM = 20190905;
     private static final int BOOT_PATCH_LEVEL_MINIMUM = 20190905;
