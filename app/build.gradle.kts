011205bf8e52607e1268394625468970fca6af1a
Daniel Micay <daniel.micay@grapheneos.org>
2024-08-29 17:02:50 -0400

    increment version to 84

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
         applicationId = "app.attestation.auditor"
         minSdk = 31
         targetSdk = 34
-        versionCode = 83
+        versionCode = 84
         versionName = versionCode.toString()
         resourceConfigurations.add("en")
     }
