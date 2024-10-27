1e6c1b4caa6421c2ac4250394d45fccb18ef6e9b
Daniel Micay <daniel.micay@grapheneos.org>
2024-10-27 07:14:39 -0400

    add check to silence false positive lint warning
    The BootReceiver class is explicitly not exported so this won't improve
    anything but the lint expects it to be here. It didn't used to report an
    issue so it was likely a regression in the check. It makes more sense to
    add the check than silencing the lint in this case.

--- app/src/main/java/app/attestation/auditor/BootReceiver.java
+++ app/src/main/java/app/attestation/auditor/BootReceiver.java
@@ -7,6 +7,9 @@ import android.content.Intent;
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, final Intent intent) {
+        if (!Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            throw new IllegalStateException("invalid action");
+        }
         RemoteVerifyJob.restore(context);
     }
 }
