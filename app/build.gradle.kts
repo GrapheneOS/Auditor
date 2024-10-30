5d78ae149d6d33e9c4cbb89ec1ffc8aa86925be1
Daniel Micay <daniel.micay@grapheneos.org>
2024-10-30 17:39:22 -0400

    update AndroidX Core to 1.15.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,7 +100,7 @@ android {
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.biometric:biometric:1.1.0")
-    implementation("androidx.core:core:1.13.1")
+    implementation("androidx.core:core:1.15.0")
     implementation("androidx.preference:preference:1.2.1")
 
     implementation("com.google.android.material:material:1.12.0")
