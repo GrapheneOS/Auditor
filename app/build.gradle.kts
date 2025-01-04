5d8af0b2dbc34fddeac7cce19f4e95f7eaadba22
Daniel Micay <daniel.micay@grapheneos.org>
2025-01-03 21:55:00 -0500

    update CameraX to 1.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -110,7 +110,7 @@ dependencies {
     implementation("com.google.zxing:core:3.5.3")
     implementation("org.bouncycastle:bcprov-jdk18on:1.79")
 
-    val cameraVersion = "1.3.4"
+    val cameraVersion = "1.4.1"
     implementation("androidx.camera:camera-core:$cameraVersion")
     implementation("androidx.camera:camera-camera2:$cameraVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraVersion")
