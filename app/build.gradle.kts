777f89399b03cd51e4e02f0be4c6de4dcf94148f
MHShetty <shettymohitn@gmail.com>
2025-09-17 04:47:55 +0530

    Upgrade to target SDK 36
    From version 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
     defaultConfig {
         applicationId = "app.attestation.auditor"
         minSdk = 33
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 90
         versionName = versionCode.toString()
     }
