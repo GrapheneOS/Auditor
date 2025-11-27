49aa94fb28994df7171e0fb7a9fd9c2dc957b3fc
Daniel Micay <daniel.micay@grapheneos.org>
2025-11-27 01:36:33 -0500

    update Bouncy Castle to 1.83

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,7 +104,7 @@ dependencies {
     implementation("com.google.android.material:material:1.13.0")
     implementation("com.google.guava:guava:33.5.0-android")
     implementation("com.google.zxing:core:3.5.4")
-    implementation("org.bouncycastle:bcprov-jdk18on:1.82")
+    implementation("org.bouncycastle:bcprov-jdk18on:1.83")
 
     val cameraVersion = "1.5.1"
     implementation("androidx.camera:camera-core:$cameraVersion")
