e8739cd1ebf3492228e05bdb2b59d8acdfb630f6
Vivek Revankar <vivek@master-hax.com>
2026-02-25 04:56:17 +0000

    naming + nits

--- app/src/main/java/app/attestation/auditor/InspectAuditeeActivity.java
+++ app/src/main/java/app/attestation/auditor/InspectAuditeeActivity.java
@@ -47,8 +47,8 @@ public class InspectAuditeeActivity extends AppCompatActivity {
         });
 
         // load auditee information
-        final String fingerprint_hex = getIntent().getStringExtra(INTENT_KEY_FINGERPRINT);
-        if (fingerprint_hex == null || fingerprint_hex.isBlank()) {
+        final String fingerprintHex = getIntent().getStringExtra(INTENT_KEY_FINGERPRINT);
+        if (fingerprintHex == null || fingerprintHex.isBlank()) {
             Log.e(TAG, "auditee fingerprint not provided");
             Toast.makeText(
                     this,
@@ -60,20 +60,19 @@ public class InspectAuditeeActivity extends AppCompatActivity {
         }
         final AttestationProtocol.AuditeeSummary summary = AttestationProtocol.getAuditorSummary(
                 this,
-                fingerprint_hex
+                fingerprintHex
         );
 
-        addSummaryFieldToTable(R.id.summary_hardware, R.string.inspect_auditee_field_name_fingerprint, fingerprint_hex);
+        // display auditee information
+        addSummaryFieldToTable(R.id.summary_hardware, R.string.inspect_auditee_field_name_fingerprint, fingerprintHex);
         addSummaryFieldToTable(R.id.summary_hardware, R.string.inspect_auditee_field_name_pinned_security_level, String.valueOf(summary.pinnedSecurityLevel()));
         addSummaryFieldToTable(R.id.summary_hardware, R.string.inspect_auditee_field_name_pinned_os_version, String.valueOf(summary.pinnedOsVersion()));
         addSummaryFieldToTable(R.id.summary_hardware, R.string.inspect_auditee_field_name_pinned_os_patch_level, String.valueOf(summary.pinnedOsPatchLevel()));
         addSummaryFieldToTable(R.id.summary_hardware, R.string.inspect_auditee_field_name_pinned_vendor_patch_level, String.valueOf(summary.pinnedVendorPatchLevel()));
         addSummaryFieldToTable(R.id.summary_hardware, R.string.inspect_auditee_field_name_pinned_boot_patch_level, String.valueOf(summary.pinnedBootPatchLevel()));
         addSummaryFieldToTable(R.id.summary_hardware, R.string.inspect_auditee_field_name_verified_boot_key, summary.verifiedBootKey());
-
         addSummaryFieldToTable(R.id.summary_os, R.string.inspect_auditee_field_name_pinned_app_version, String.valueOf(summary.pinnedAppVersion()));
         addSummaryFieldToTable(R.id.summary_os, R.string.inspect_auditee_field_name_pinned_app_variant, String.valueOf(summary.pinnedAppVariant()));
-
         addSummaryFieldToTable(R.id.summary_history, R.string.inspect_auditee_field_name_first_verified, new Date(summary.verifiedTimeFirst()).toString());
         addSummaryFieldToTable(R.id.summary_history, R.string.inspect_auditee_field_name_last_verified, new Date(summary.verifiedTimeLast()).toString());
     }
